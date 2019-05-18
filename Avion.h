07bc87b34106bf3c802334f8f20db07fb1661a06
lizduran <42351387+lizduran@users.noreply.github.com>
2019-05-18 14:41:52 -0500

    Update Avion.h

--- Avion.h
+++ Avion.h
@@ -1,3 +1,11 @@
+/**
+ * @file Avion.h
+ * @Author Team
+ * @date mayo 2019
+ * @brief Proyecto - EDA-I
+ * @note Grupo: 0007, Semestre 2019-2
+ */
+ 
 #ifndef _AVION_H_
 #define _AVION_H_
 
@@ -8,33 +16,56 @@
 #include <string.h>
 #include <time.h>
 
-
+/**
+ * @typedef Avion de tipo estructura.
+ * @struct Avion 
+ * @brief campos en Avion
+ */
 typedef struct
 {
 	char _nombre[20];
 	int pasajeros;
 	int capacidad;
-	int estado;//1=ocupado,2=disponnible,3=dañado
+	int estado; //1=ocupado,2=disponible,3=danado
 	int hora_salida;//en segundos
 	int hora_llegada;//en segundos
-
 }Avion;
 
+
+/**
+ * @typedef Item tipo Avion.
+ */
 typedef Avion Item;
+
+/**
+ * @typedef ItemPtr aputador a tipo de dato.
+ */
 typedef Item* ItemPtr;
 //data para la lista DLL
 
-/**Esta función resetea los valores pasajeros, hora_llegada,
- *  hora_salida a 0 y estado a 1(disponible)
-*/
+
+
+/**
+ * @brief Resetea los valores hora_llegad,hora_salida,pasajeros
+ *  y estado de un avion.
+ * @param apuntador a tipo avion.
+ */
 void Avion_Reset(ItemPtr this);
-/**Esta función asigna valores al compo pasajeros, hora_llegada,
- *  hora_salida y estado de un avión
-*/
+
+
+/**
+ * @brief Asigna valores a los campos:hora_llegad,hora_salida,pasajeros
+ *  y estado de un avion.
+ * @param apuntador a tipo Avion, valor de: hora_llegada, hora_salida
+ */
 void Avion_Llenar(ItemPtr this,int _hora_llegada, int _hora_salida);
-/**Esta función libera la memoria utilizada por el avion
-*/
+
+
+/**
+ * @brief Libera memoria utilizada por un avion.
+ * @param apuntador a tipo Avion.
+ */
 void Avion_Delete(ItemPtr this);
 
 
-#endif
+#endif  // _AVION_H_
