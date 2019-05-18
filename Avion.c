734987be53ca00fcafeb25de4a1da090676ad9b7
lizduran <42351387+lizduran@users.noreply.github.com>
2019-05-18 14:42:26 -0500

    Update Avion.c

--- Avion.c
+++ Avion.c
@@ -1,11 +1,6 @@
-/***------API Avión--------***/
+/***------API Avion--------***/
 
-/**
- * @brief Resetea los valores hora_llegad,hora_salida,pasajeros
- *  y estado de un avion.
- * @param apuntador a un avión
- * @return nada
- */
+
 void Avion_Reset(ItemPtr this){
 
 	 this->pasajeros=0;
@@ -13,29 +8,21 @@ void Avion_Reset(ItemPtr this){
 	 this->hora_salida=0;
 	 this->estado=2;
 }
-/**
- * @brief Asigna valores a los campos:hora_llegad,hora_salida,pasajeros
- *  y estado de un avion.
- * @param apuntador a un avión, el valor de: hora_llegada, hora_salida
- * @return nada
- */
+
+
 void Avion_Llenar(ItemPtr this,int _hora_llegada, int _hora_salida){
 
 	srand( time( 0 ) );
-    // inicializamos al generador de números aleatorios
+    // inicializamos al generador de nÃºmeros aleatorios
 	 int capacidad=this->capacidad;
-	 // genera un número aleatorio en el rango 0..capacidad del avion
+	 // genera un nÃºmero aleatorio en el rango 0..capacidad del avion
 	 int value = rand() % capacidad;
      this->pasajeros=value;
 	 this->hora_llegada=_hora_llegada;
 	 this->hora_salida=_hora_salida;
 	 this->estado=1;
 }
-/**
- * @brief Libera memoria utilizada por un avión
- * @param apuntador a un avión
- * @return nada
- */
+
 void Avion_Delete(ItemPtr this){
      if( this ){
        
